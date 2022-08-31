6bf97b68e7b264a028ddd8b9cffa366cbef631c1
Gtodev <60323923+Gtodev123@users.noreply.github.com>
2022-08-31 17:57:20 +0300

    Add more section components
    FAQ section and Services Info

---
+++ gtt/src/components/FaqSection.js
@@ -0,0 +1,34 @@
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+        <h2>Any Questions ? <span>FAQ</span></h2>
+<div className="question">
+    <h4>how do i startr</h4>
+    <div className="answer">
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+    </div>
+</div>
+<div className="question">
+    <h4>Daily schedule</h4>
+    <div className="answer">
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+    </div>
+</div>
+<div className="question">
+    <h4>Different Payment Methods</h4>
+    <div className="answer">
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+    </div>
+    <div className="question">
+    <h4>What product do you offer?</h4>
+    <div className="answer">
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+    </div>
+</div>
+</div>
+</div>
+    )
+}
+
+export default FaqSection
